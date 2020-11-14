b775168a96fcd9f58e38a8e1f7d41b3421ef591c
Ask Hetland Berentsen <askberentsen@gmail.com>
2020-11-14 21:30:16 +0100

    tryna do some stuff

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,5 +1,6 @@
 # preprocesses the data
 import re
+import random
 
 
 def process_line(text):
@@ -23,3 +24,18 @@ if __name__ == '__main__':
     with open("../data/real.csv", "w") as data_file:
         data_file.write(labels + "\n" + ("\n".join(real_data)))
     print("success!")
+
+
+def split_training_data(data: list, anomalous_data: list, training_size: float, test_bias: float):
+    tmp_data, tmp_anomalous_data = list(data), list(anomalous_data)
+
+
+    training_data, test_data = [], []
+    for item in anomalous_data:
+        ls = training_data if random.random() < training_size else test_data
+        ls.append(item)
+
+    # normal dist:
+
+
+    return training_data, test_data
