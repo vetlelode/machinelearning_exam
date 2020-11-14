540f37fad48ebb385e909180ed7f8e4ebbf6808c
Vetle Lode <vetlelode@gmail.com>
2020-11-14 21:02:38 +0100

    Update KNN to use a 50/50 split of the dataset

--- prototypes/knn.py
+++ prototypes/knn.py
@@ -8,17 +8,25 @@ from sklearn.preprocessing import StandardScaler
 
 # Simple prototype of KNN on the dataset
 
-data_raw = pd.read_csv("../data/creditcard.csv")
-X = data_raw.iloc[:, :-1].values
-Y = data_raw.iloc[:, 30].values
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
+#data_raw = pd.read_csv("../data/creditcard.csv")
+fake = pd.read_csv("../data/fake.csv")
+real = pd.read_csv("../data/real.csv")
 
+real = real.sample(n=492)
+combined = pd.concat([fake, real])
+combined = combined.sample(frac=1)
+print(combined)
+X = combined.iloc[:, :-1].values
+Y = combined['Class']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
+print(Y_train.shape)
 #scaler = StandardScaler()
 # scaler.fit(X_train)
 #X_train = scaler.transform(X_train)
 #X_test = scaler.transform(X_test)
 
-classifier = KNeighborsClassifier(n_neighbors=5)
+classifier = KNeighborsClassifier()
 classifier.fit(X_train, Y_train)
 
 Y_pred = classifier.predict(X_test)
