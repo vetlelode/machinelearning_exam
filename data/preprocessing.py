2d2f9861e9844542e1fd81216ad04487f1f11385
Ask Hetland Berentsen <askberentsen@gmail.com>
2020-12-12 16:19:29 +0100

    Added an option

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -15,8 +15,9 @@ def process_lines(lines: list) -> list:
     return [[float(f) for f in process_line(line).split(",")] for line in lines[1:]]
 
 
-def split_training_data(data: list, anomalous_data: list) -> tuple:
-    k = len(anomalous_data)//2
+def split_training_data(data: list, anomalous_data: list, f: float = 0.5) -> tuple:
+    k = int((1-f) * len(anomalous_data))
+
     test_rdata, training_rdata = sample_split(data, k)
     test_adata, training_adata = sample_split(anomalous_data, k)
     test_data = test_adata + test_rdata
@@ -38,11 +39,12 @@ def sample_split(data: list, k: int) -> tuple:
     return list_1, list_2
 
 
-def get_dataset(k1: int, k2: int):
+def get_dataset(k1: int, k2: int, f: float = 0.5):
     """
     Fetches the dataset and splits into training data and test data
     :param k1: the amount of entries to read from the real data
     :param k2: the amount of entries to read from the anomalous data
+    :param f: how much of the anomalous data should be in the training data (0<=f<=1)
     :return: training_data, test_data
     """
     if files_absent():
@@ -61,7 +63,7 @@ def get_dataset(k1: int, k2: int):
         lines = [next(data_file) for _ in range(k2+1)]
         anomalous_data += process_lines(lines)
 
-    return split_training_data(real_data, anomalous_data)
+    return split_training_data(real_data, anomalous_data, f)
 
 
 def preprocess_files():
@@ -100,4 +102,7 @@ if __name__ == '__main__':
         print("success!")
     else:
         print("ok")
-
+        training_data, test_data = get_dataset(1000, 100, 0)
+        print(len(training_data), len(test_data))
+        print(sum([line[-1] for line in training_data]))
+        print(sum([line[-1] for line in test_data]))
