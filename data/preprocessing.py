e1fda061b92d1d7ccc8e9683c11d80dac29fd642
Ask Hetland Berentsen <askberentsen@gmail.com>
2020-12-12 15:56:39 +0100

    Added better preprocessing

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,12 +2,62 @@
 import re
 import random
 
+REAL_DATA_MAX_N: int = 284807
+FAKE_DATA_MAX_N: int = 492
 
-def process_line(text):
+def process_line(text: str) -> str:
     return re.sub("[\"\n]", "", text)
 
+def process_lines(lines: list) -> list:
+    return [[float(f) for f in process_line(line).split(",")] for line in lines[1:]]
 
-if __name__ == '__main__':
+def split_training_data(data: list, anomalous_data: list) -> tuple:
+    k = len(anomalous_data)//2
+    test_rdata, training_rdata = sample_split(data, k)
+    test_adata, training_adata = sample_split(anomalous_data, k)
+    test_data = test_adata + test_rdata
+    training_data = training_adata + training_rdata
+    random.shuffle(test_data)
+    random.shuffle(training_data)
+    return training_data, test_data
+
+def sample_split(data: list, k: int) -> tuple:
+    tmp = list(data)
+    list_1, list_2 = [], []
+    n = len(data)
+    for i in range(min(len(data), k)):
+        index = random.randint(0, n-i-1)
+        list_1 += [tmp.pop(index)]
+    random.shuffle(tmp)
+    list_2 += tmp
+    return list_1, list_2
+
+def get_dataset(k1: int, k2: int):
+    """
+    Fetches the dataset and splits into training data and test data
+    :param k1: the amount of entries to read from the real data
+    :param k2: the amount of entries to read from the anomalous data
+    :return: training_data, test_data
+    """
+    if files_absent():
+        print("preprocessing files. This may take a while")
+        preprocess_files()
+        print("done!")
+
+    k1 = min(k1, REAL_DATA_MAX_N)
+    k2 = min(k2, FAKE_DATA_MAX_N)
+    anomalous_data, real_data = [], []
+    with open("real.csv", "r") as data_file:
+        lines = [next(data_file) for _ in range(k1+1)]
+        real_data += process_lines(lines)
+
+    with open("fake.csv", "r") as data_file:
+        lines = [next(data_file) for _ in range(k2+1)]
+        anomalous_data += process_lines(lines)
+
+    return split_training_data(real_data, anomalous_data)
+
+def preprocess_files():
     with open("../data/creditcard.csv", "r") as data_file:
         lines = data_file.readlines()
         labels = process_line(lines[0])
@@ -23,19 +73,19 @@ if __name__ == '__main__':
 
     with open("../data/real.csv", "w") as data_file:
         data_file.write(labels + "\n" + ("\n".join(real_data)))
-    print("success!")
 
+def files_absent():
+    try:
+        with open("real.csv", "r"):
+            pass
+    except IOError:
+        return True
+    else:
+        return False
 
-def split_training_data(data: list, anomalous_data: list, training_size: float, test_bias: float):
-    tmp_data, tmp_anomalous_data = list(data), list(anomalous_data)
-
-
-    training_data, test_data = [], []
-    for item in anomalous_data:
-        ls = training_data if random.random() < training_size else test_data
-        ls.append(item)
-
-    # normal dist:
-
-
-    return training_data, test_data
+if __name__ == '__main__':
+    if input("preprocess the files? y/n ") == "y":
+        preprocess_files()
+        print("success!")
+    else:
+        print("ok")
