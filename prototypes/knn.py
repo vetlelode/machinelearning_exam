f997c2db48a6795c9202107ccd62d92a0a739348
Vetle Lode <vetlelode@gmail.com>
2020-12-12 14:38:55 +0100

    newest ver

--- prototypes/knn.py
+++ prototypes/knn.py
@@ -13,11 +13,17 @@ Prototype implmentation of an KNN based solution with a 50/50 dataset
 
 fake = pd.read_csv("../data/fake.csv")
 real = pd.read_csv("../data/real.csv")
-real = real.sample(n=492)
-combined = pd.concat([fake, real])
+
+# Create two dataset one with all the data and one split 50/50
+
+full = pd.concat([fake, real])
+full = full.sample(frac=1)
+print(full)
+
+combined = pd.concat([fake, real.sample(n=492)])
 combined = combined.sample(frac=1)
+print(combined)
 X = combined.iloc[:, :-1]
-print(X)
 Y = combined['Class']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
@@ -31,22 +37,16 @@ classifier.fit(X_train, Y_train)
 Y_pred = classifier.predict(X_test)
 
 # Print out various metrics about the accuracy on a 50/50 dataset
-print(classifier.score(X_test, Y_test))
 print(confusion_matrix(Y_test, Y_pred))
 print(classification_report(Y_test, Y_pred))
 
 
 # Only run on the full dataset if -f is provided as a argument
 if len(sys.argv) >= 2 and sys.argv[1] == "-f":
+    print(len(real))
     print("Running the KNN classifier on the full dataset:")
-    full = pd.read_csv("../data/creditcard.csv")
-    print(combined.shape)
-    print(full.shape)
     X_test_full = full.iloc[:, :-1]
-    print(X_test_full)
     X_test_full = scaler.transform(X_test_full)
     Y_test_full = full['Class']
-    print(full)
     Y_pred_full = classifier.predict(X_test_full)
-
     print(classification_report(Y_test_full, Y_pred_full))
