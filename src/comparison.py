e00a58b6eb112fdd3c3c287e5bd765e2a4ae632f
Vetle Lode <vetlelode@gmail.com>
2020-12-15 20:03:17 +0100

    etc

--- src/comparison.py
+++ src/comparison.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 def runComp():
-    X_train, Y_train, X_test, Y_test = get_dataset(4000, 400, 0.5)
+    X_train, Y_train, X_test, Y_test = get_dataset(5000, 200, 0.5)
     cols = ["Time", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15",
             "V16", "V17", "V18", "V19", "V20", "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "Amount"]
     X_train = pd.DataFrame(X_train, columns=cols)
@@ -18,16 +18,23 @@ def runComp():
     Y_pred = runKNN(X_train, Y_train, X_test, Y_test)
     cf_knn = confusion_matrix(Y_test, Y_pred)
     print("Confusion matrix for KNN:\n{}".format(cf_knn))
+    print("False positive rate: {}%".format(
+        cf_knn[1][0]/len(Y_test)))
     if len(sys.argv) >= 2:
         if sys.argv[1] == "-grid":
             findBestParams(X_train, Y_train, X_test, Y_test)
         elif sys.argv[1] == "-corr":
+            # Print out a correlation matrix for the entire dataset, allowing some limited insight into the correlation of the attributes to the class
             correlationMatrix()
         elif sys.argv[1] == "-nca":
-            Y_pred_pca = knn_NCA(X_train, Y_train, X_test, Y_test, 1)
-            cf_knn_pca = confusion_matrix(Y_test, Y_pred_pca)
-            print("Confusion matrix for KNN with NCA:\n{}".format(cf_knn_pca))
+            # Run the KNN classifier, but with NCA dimensionalty reduction, which from our testing gives slightly better results than PCA
+            Y_pred_nca = knn_NCA(X_train, Y_train, X_test, Y_test, 1)
+            cf_knn_nca = confusion_matrix(Y_test, Y_pred_nca)
+            print("Confusion matrix for KNN with NCA:\n{}".format(cf_knn_nca))
+            print("False positive rate: {}".format(
+                cf_knn_nca[1][0]/len(Y_test)))
         elif sys.argv[1] == "-dim":
+            # Function comparing the results of PCA and NCA
             dim_reduc(X_train, Y_train, X_test, Y_test, 1)
 
 
