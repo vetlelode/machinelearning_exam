693a3c375dcf275cf38ee09a58448181958ebad4
Ask Hetland Berentsen <askberentsen@gmail.com>
2020-12-23 17:35:08 +0100

    Some minor things

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from preprocessing import get_dataset
+from preprocessing import get_dataset, REAL_DATA_MAX_N
 from sklearn.neural_network import MLPRegressor
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix, r2_score
@@ -10,9 +10,9 @@ from scipy.stats import invgamma
 
 
 #Hyperparameters:
-hidden_layers = [20,20,10,2,10,20,20]
-components = 28
-activation = "tanh" #or relu
+training_size = REAL_DATA_MAX_N//10 #284315
+hidden_layers = [20,10,2,10,20]
+activation = "tanh" #or relu. Tanh works best (and giges the nicest graphs!)
 # Higher threshold means less false positives, but also less true negatives
 threshold = 0.9
 
@@ -26,7 +26,7 @@ def identity(x):
 
 
 #Diagrams - Details unimportant
-def plot_histogram(X, Y, title, threshold=None) -> None:
+def plot_histogram(X, Y, title, threshold : float = None) -> None:
     #Omit extreme outliers
     std = np.std(X)
     mean = np.mean(X)
@@ -79,15 +79,14 @@ def plot_latent(network, Xs, cols=None, alphas=None, labels = None):
     plt.show()
 
 def plot_gamma(scores, p, threshold, title, scale=0.3):
-    #
-    max_x = min(max(scores), np.mean(scores)+3*np.std(scores))
-    bins = np.linspace(0, max_x, 150)
+    max_x = min(max(scores)*1.5, np.mean(scores)+3*np.std(scores))
+    bins = np.linspace(0, max_x, 250)
     fig, ax = plt.subplots(figsize=(15,15))
     # Plot the training scores
-    ax.hist(scores, bins, density=True)
+    ax.hist(scores, bins, density=True, stacked=True)
     # Plot the describing gamma distribution (not to scale)
     min_ylim, max_ylim = plt.ylim()
-    ax.plot(bins, np.multiply(invgamma.pdf(bins, *p), max_ylim*scale))
+    ax.plot(bins, invgamma.pdf(bins, *p))
     ax.vlines(threshold, min_ylim, max_ylim, color="black")
     plt.title(title)
     plt.show()
@@ -173,13 +172,17 @@ def gamma_threshold(scores, threshold):
         a, loc, scale = invgamma.fit(scores)
         return invgamma.isf(1-threshold, a, loc, scale), (a, loc, scale)
 
+
 def plot_scores(scores, Y, title, threshold=None):
     plot_histogram( scores, Y, title, threshold)
-    plot_scatter  ( scores, Y, title, threshold)
+    plot_scatter  ( np.log(scores), Y, f"log({title})", np.log(threshold))
+
+
+# MAIN PROGRAM
 
 
 # Obtain the dataset
-train_X, train_Y, test_X, test_Y = get_dataset(k1=8000,f=0)
+train_X, train_Y, test_X, test_Y = get_dataset(k1=training_size,f=0)
 
 # Scale data to make training easier
 # -
@@ -193,12 +196,14 @@ scaler = StandardScaler().fit(train_X)
 train_X = scaler.transform(train_X)
 test_X = scaler.transform(test_X)
 
+inliers  = [x for x,y in zip(test_X,test_Y) if y==0]
+outliers = [x for x,y in zip(test_X,test_Y) if y==1]
 
 # Train a neural net into accurately recreate the input data 
 # through a small latent space.
 auto_encoder = MLPRegressor(
     solver="adam",
-    activation=activation, #Relu also works well, but easily leads to dead neurons
+    activation=activation, 
     hidden_layer_sizes = hidden_layers,
     warm_start=False, #Used in debugging
     max_iter=50,
@@ -212,15 +217,12 @@ auto_encoder = MLPRegressor(
 auto_encoder.fit(train_X, train_X)
 
 
-# Plot out the latent space
+# Extract the network architecture from the autoencoder
 n_layers = len(auto_encoder.coefs_)
-
 activations = [np.tanh if activation =="tanh" else relu]*(n_layers-1)+[identity]
 network = list(zip(auto_encoder.coefs_, auto_encoder.intercepts_, activations))
 
-inliers  = [x for x,y in zip(test_X,test_Y) if y==0]
-outliers = [x for x,y in zip(test_X,test_Y) if y==1]
-
+# Plot out the latent space (Pretty!)
 plot_latent(network, Xs=(train_X,inliers,outliers), alphas=(0.2,0.5,0.5), cols=("blue","green","black"), labels=("training data","inliers","outliers"))
 
 
@@ -228,31 +230,29 @@ plot_latent(network, Xs=(train_X,inliers,outliers), alphas=(0.2,0.5,0.5), cols=(
 train_recreations = auto_encoder.predict( train_X )
 test_recreations  = auto_encoder.predict( test_X  )
 
+
 # Score the samples with r2. This is the loss function used in the autoencoder.
 # This is done because sklearn auto encoder does not support per sample scoring
 train_r2_scores = [r2_score( x_true, x_pred ) for x_true, x_pred in zip(train_X, train_recreations)]
 test_r2_scores  = [r2_score( x_true, x_pred ) for x_true, x_pred in zip(test_X, test_recreations)]
 
 
+# Adjust the scores - Shift it so all the scores are positive
 mm = max(train_r2_scores)
 train_r2_scores = -(train_r2_scores - mm)
 test_r2_scores = -(test_r2_scores - mm)
 
+# Calculate the threshold, assuming the distribution of training scores
+# follow the gamma distribution.
+# We have seen that the gamma threshold gives a better scoring than
+# that of a normal distribution threshold.
 r2_threshold, r2_p = gamma_threshold(train_r2_scores,threshold)
-
-plot_gamma(train_r2_scores, r2_p, r2_threshold, "training r2 scores", 0.3)
-
-# The scores from the training follows the gamma distribution, but the ones from
-
-
-
-# Find a decent threshold to split inliers and outliers
-# Use the 68% rule (1 standard deviation)
-#r2_threshold = np.mean(train_r2_scores) + np.std(train_r2_scores)
 
 # Predict the test data wrt the r2 threshold
 r2_predictions = [1 if x > r2_threshold else 0 for x in test_r2_scores]
 
+# Plot the gamma distribution of best fit
+plot_gamma(train_r2_scores, r2_p, r2_threshold, "training r2 scores")
 
 # Plot the r2 scores
 plot_scores(test_r2_scores, test_Y, 'R2 scores',r2_threshold)
@@ -270,62 +270,78 @@ test_errors     = test_X  - test_recreations
 
 
 # With log-likelihood the variance of the axes are taken into account
+# AELL - Auto-Encoder-Log-Likelihood
 ae_LL = LogLikelihood(training_errors)
 
-# Score the training data. These should have a low mean score
+# Score the data.
 training_aell_scores = ae_LL.score(training_errors)
 test_aell_scores, aell_threshold, aell_predictions, aell_p = ae_LL.predict(test_errors,threshold)
 
 
-# Some plotting
-plot_gamma(training_aell_scores, aell_p, aell_threshold, "training AE-LL scores", 20)
+# Plot the gamma distribution of best fit
+plot_gamma(training_aell_scores, aell_p, aell_threshold, "training AE-LL scores")
 
 # Plot the ll scores
 plot_scores(
-        np.log(test_aell_scores), 
+        test_aell_scores, 
         test_Y, 
         'log(log AE likelihood scores)', 
-        np.log(aell_threshold)
+        aell_threshold
         )
 
+
+# Log-Likelihood is a statistical model we can use directly on the data as well,
+# And we need to check if the Log-Likelihood performs as well, worse or better
+# on the recreation error than on the raw data.
+# If it performs as well or better, the AE is a redundant step.
 
 direct_LL = LogLikelihood(train_X)
 
-# Score the training data. These should have a low mean score
+# Score the data. 
 training_dll_scores = ae_LL.score(training_errors)
 test_dll_scores, dll_threshold, dll_predictions, dll_p = ae_LL.predict(test_X,threshold)
 
-plot_gamma(training_dll_scores, dll_p, dll_threshold, 20)
+# Plot the gamma distribution of best fit
+plot_gamma(training_dll_scores, dll_p, dll_threshold, "Direct Log Likelihood scores", 20)
 plot_scores(
-        np.log(test_dll_scores), 
+        test_dll_scores, 
         test_Y, 
         'log(log direct likelihood scores)', 
-        np.log(dll_threshold)
+        dll_threshold
         )
 
 # It seems r2 scoring performs better when we sample more data
-
+# The statistical model fitted from the training data, used to set a threshold,
+# is not too good of a fit and produces more false negatives than we would
+# like
 print("r2 report:")
 print(confusion_matrix(test_Y, r2_predictions))
 print(classification_report(test_Y, r2_predictions))
 
-
 # But AE-LL has stable performance, even when undersampling
+
+# It seems the statistical model fitted on the training set
+# is also a good fit for the testing data. There are about the
+# same proportion of true false positives to true positives as
+# false negatives to true negatives
 print("AE-LL report:")
 print(confusion_matrix(test_Y, aell_predictions))
 print(classification_report(test_Y, aell_predictions))
 
-# Direct LL gives usable results, but perform poorer with more data
+# Direct LL gives usable results, but are consistently worse than when applied
+# to the recreation error
 print("direct-LL report:")
 print(confusion_matrix(test_Y, dll_predictions))
 print(classification_report(test_Y, dll_predictions))
+
+
+# Covariance analysis
 
 inlier_errors = [x for x,y in zip(test_errors,test_Y) if y==0]
 outlier_errors = [x for x,y in zip(test_errors,test_Y) if y==1]
 
 # The recreation error of inliers are for the mostly uncorrelated
 plt.matshow(pd.DataFrame(inlier_errors).corr())
-plt
 plt.title("Correlation matrix: inlier errors")
 plt.show()
 
