23752d509bf7113e1b7a0342559922ddf083958c
Vetle Mæland Lode <vetlelode@gmail.com>
2020-12-28 19:43:46 +0100

    Paramater estimation is working now, but transformation is still bugged

--- src/scaler.py
+++ src/scaler.py
@@ -1,19 +1,21 @@
 import numpy as np
 from preprocessing import get_dataset
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler as Skaler
+from math import sqrt
 
 
 class StandardScaler:
     """
     Our implementation of an Sklearn style scaler
     Scales the dataset using the formula:
-    z = (x - u) / s
+    z = (x - u) / s^2
     where x is the original sample, u is the mean value of the column,
     and s is the standard deviation of the column
     """
 
     def __init__(self):
-        self.var = 0
-        self.mean = 0
+        self.scaler_vars = np.empty((30, 2))
 
     def fit(self, X: np.ndarray, y=None):
         """
@@ -24,19 +26,21 @@ class StandardScaler:
         for i in range(len(X[0])):
             column = X[:, i]
             # U
-            self.mean = np.mean(column)
+            self.scaler_vars[i, 0] = np.mean(column)
             # S
-            self.var = np.std(column)
+            self.scaler_vars[i, 1] = np.std(column, ddof=0)**2
 
     def transform(self, X):
         """
         Transform the provided dataset X using the earlier fit
         """
+        print(self.scaler_vars[1])
         X = np.array(X)
+        X_new = np.empty(X.shape)
         for i in range(len(X)):
             for j in range(len(X[0])):
-                X[i, j] = (
-                    X[i, j]-self.mean)/self.var
+                X_new[i, j] = (
+                    X[i, j]-self.scaler_vars[j][0])/self.scaler_vars[j][1]
         return X
 
     def fit_transform(self, X):
@@ -49,13 +53,37 @@ class StandardScaler:
 
     def _reset(self):
         "Reset everything"
-        del self.var
-        del self.mean
+        self.scaler_vars = np.empty((30, 2))
 
 
 if __name__ == "__main__":
+    """
+    Essentially just debugging code used to test the scaler versus the Sklearn one
+    """
     X_train, Y_train, X_test, Y_test = get_dataset(
         sample=1000, pollution=0.7, train_size=0.9)
+    # Test our awsome scaler
     scaler = StandardScaler()
-    scaler.fit_transform(X_train)
-    scaler.transform(X_test)
+    X_train_0 = X_train
+    X_test_0 = X_test
+    X_train_1 = X_train
+    X_test_1 = X_test
+    scaler.fit(X_train_0)
+    X_train_0 = scaler.transform(X_train_0)
+    X_test_0 = scaler.transform(X_test_0)
+    # Test the boring Sklearn scaler
+    skaler = Skaler()
+    X_train_1 = skaler.fit_transform(X_train_1)
+    X_test_1 = skaler.transform(X_test_1)
+
+    # Check for deviations from the standard Sklearn scaler
+    failed = False
+    for i in range(len(X_train_0[0])):
+        if(round(scaler.scaler_vars[i][0], 4) != round(skaler.mean_[i], 4)):
+            failed = True
+            print(scaler.scaler_vars[i][0], skaler.mean_[i])
+        elif (round(scaler.scaler_vars[i][1], 4) != round(skaler.var_[i], 4)):
+            failed = True
+            print(scaler.scaler_vars[i][1], skaler.var_[i])
+    if failed == False:
+        print("Sucess the two scalers are more or less the same!")
